6be44980f30930052a02d0828897aff88106191b
Dhanraj D Hasure <128172922+dhanrajdhasure7143@users.noreply.github.com>
2025-05-14 20:07:14 +0530

    Update HelloWorld.java

--- HelloWorld/src/com/progrmas/HelloWorld.java
+++ HelloWorld/src/com/progrmas/HelloWorld.java
@@ -3,5 +3,6 @@ package com.progrmas;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Learning GitHub..");
+		System.out.println("Added comment in GitHub UI");
 	}
 }
